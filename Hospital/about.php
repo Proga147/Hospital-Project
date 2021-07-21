aa07e1c1eea80bd51dee2e7a4120d904af0e3e79
Shane Proverbs <shane.306@hotmail.com>
2021-07-20 23:04:49 -0400

    Add files via upload

---
+++ Hospital/about.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+require("includes/config.php");
+$loginStatus = new applicationHelperClass();
+$title = 'about';
+?>
+
+
+<?php include_once('header.php'); ?>
+
+
+<div class="container bg-light text-center">
+    <div class="container">
+
+        <br><br>
+        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim magnam ipsam eos aperiam atque
+            voluptatem consequatur pariatur fuga,
+            ad ipsum veritatis quos perspiciatis reiciendis excepturi explicabo quae nostrum cumque officiis. Lorem
+            ipsum dolor sit amet, consectetur
+            adipisicing elit. Deleniti dolorem nisi voluptatibus. Repudiandae maiores ratione reiciendis, neque nesciunt
+            nisi reprehenderit cumque, perferendis
+            consequatur temporibus esse aspernatur beatae corporis ea! Saepe.Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Enim magnam ipsam eos aperiam atque voluptatem consequatur pariatur fuga,
+            ad ipsum veritatis quos perspiciatis reiciendis excepturi explicabo quae nostrum cumque officiis. Lorem
+            ipsum dolor sit amet, consectetur
+            adipisicing elit. Deleniti dolorem nisi voluptatibus. Repudiandae maiores ratione reiciendis, neque nesciunt
+            nisi reprehenderit cumque, perferendis
+            consequatur temporibus esse aspernatur beatae corporis ea! Saepe.Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Enim magnam ipsam eos aperiam atque voluptatem consequatur pariatur fuga,
+            ad ipsum veritatis quos perspiciatis reiciendis excepturi explicabo quae nostrum cumque officiis. Lorem
+            ipsum dolor sit amet, consectetur
+            adipisicing elit. Deleniti dolorem nisi voluptatibus. Repudiandae maiores ratione reiciendis, neque nesciunt
+            nisi reprehenderit cumque, perferendis
+            consequatur temporibus esse aspernatur beatae corporis ea! Saepe.</p>
+    </div>
+    <br><br>
+
+    <div class="container">
+        <h4>Vision</h4>
+        <p class="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim magnam ipsam eos aperiam atque voluptatem
+            consequatur pariatur fuga,
+            ad ipsum veritatis quos perspiciatis reiciendis excepturi explicabo quae nostrum cumque officiis. Lorem
+            ipsum dolor sit amet, consectetur
+            adipisicing elit. Deleniti dolorem nisi voluptatibus. Repudiandae maiores ratione reiciendis, neque nesciunt
+            nisi reprehenderit cumque, perferendis
+            consequatur temporibus esse aspernatur beatae corporis ea! Saepe.</p>
+    </div>
+
+</div>
+<br><br><br><br><br><br><br>
+
+
+
+<?php include_once('footer.php'); ?>
